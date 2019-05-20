7d34aa13a78ce62ebc71549331fb9a10dbe8d696
Willard <sven0215@hotmail.com>
2019-05-20 16:26:51 +0200

    joint appveyor test

--- tools/packageLinux.sh
+++ tools/packageLinux.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-repoName=${TRAVIS_REPO_SLUG#*/}
+repoName=${APPVEYOR_PROJECT_NAME}
 
 mkdir -p @${repoName}/addons
 mkdir -p @${repoName}/intercept
 
-cp src/${repoName}.so @${repoName}/intercept/${repoName}.so
+cp src/${repoName}_x64.so @${repoName}/intercept/${repoName}_x64.so
 cp addons/${repoName}.pbo @${repoName}/addons/${repoName}.pbo
 
 zip -r @${repoName}.zip @${repoName}
