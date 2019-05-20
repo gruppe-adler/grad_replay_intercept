cf892c1eeb91b92687dfabda216876435580f9c1
Willard <sven0215@hotmail.com>
2019-05-20 17:54:40 +0200

    appveyor

--- tools/packageLinux.sh
+++ tools/packageLinux.sh
@@ -10,4 +10,13 @@ cp addons/${repoName}.pbo @${repoName}/addons/${repoName}.pbo
 
 zip -r @${repoName}.zip @${repoName}
 
+$tagToAdd = ""
+if [[ ! -z "$APPVEYOR_REPO_TAG_NAME" ]] {
+    Write-Host "Using Tag: $APPVEYOR_REPO_TAG_NAME"
+    $tagToAdd = "$APPVEYOR_REPO_TAG_NAME"
+} else {
+    $tagToAdd = "$APPVEYOR_REPO_COMMIT"
+    Write-Host "Tag not found, using commit hash"
+}
+appveyor PushArtifact @${repoName}_$tagToAdd.zip" -DeploymentName zip
 echo "Done"
