ee76ef12e319cb4840dc0bcb0c27ef27970ee3a4
Willard <sven0215@hotmail.com>
2019-05-21 20:22:04 +0200

    ci

--- tools/packageLinux.sh
+++ tools/packageLinux.sh
@@ -8,8 +8,6 @@ mkdir -p @${repoName}/intercept
 cp src/${repoName}_x64.so @${repoName}/intercept/${repoName}_x64.so
 cp addons/${repoName}.pbo @${repoName}/addons/${repoName}.pbo
 
-zip -r @${repoName}.zip @${repoName}
-
 tagToAdd=""
 if [ ! -z "$APPVEYOR_REPO_TAG_NAME" ]
 then
@@ -19,5 +17,6 @@ else
     tagToAdd="$APPVEYOR_REPO_COMMIT"
     echo "Tag not found, using commit hash"
 fi
+zip -r "@${repoName}_$tagToAdd.zip" @${repoName}
 appveyor PushArtifact "@${repoName}_$tagToAdd.zip" -DeploymentName zip
 echo "Done"
